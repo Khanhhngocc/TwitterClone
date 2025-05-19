21584bcaeae81923f6fc8966b0e17b35122e711b
Khanhhngocc <ngoctaehyung0630@gmail.com>
2025-05-19 11:09:32 +0700

    Sửa sang ES6

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,7 +2,7 @@ const homeRoutes = require("./home.route");
 const loginRoutes = require("./auth.route");
 
 // Route Api
-const postsApiRoute = require("./api/posts");
+const postsApiRoute = require("./api/post.route");
 
 
 module.exports = (app) => {
